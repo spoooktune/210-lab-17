a01f78f27c3f2df4086e084aa873a250acc91772
spoooktune <asriprasana5@gmail.com>
2025-10-02 20:56:09 -0700

    Started defining addBack() function

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Node {
 
 // prototypes (may be changed later)
 Node * addFront(Node *newNode, float val);
-void addBack(Node *newNode, float val);
+Node * addBack(Node *newNode, float val);
 Node * deleteNode (Node *headNode);
 Node * insertNode (Node *headNode, float val);
 Node * deleteList(Node *headNode);
@@ -21,103 +21,22 @@ void output(Node *hd);
 
 int main() {
     Node *head = nullptr;
-    int count = 0;
+    int index = 0;
     // create a linked list of size SIZE with random numbers 0-99
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE-1; i++) {
         int tmp_val = rand() % 100;
         head = addFront(head, tmp_val);
-
-        /*
-        Node *newVal = new Node;
-        
-        // adds node at head
-        if (!head) { // if this is the first node, it's the new head
-            head = newVal;
-            newVal->next = nullptr;
-            newVal->value = tmp_val;
-        }
-        else { // its a second or subsequent node; place at the head
-            newVal->next = head;
-            newVal->value = tmp_val;
-            head = newVal;
-        }
-        */
     }
+
+    head = addBack(head, 9800);
     output(head);
 
-    /*
-    // deleting a node
-    Node * current = head;
-    cout << "Which node to delete? " << endl;
-    output(head);
-    int entry;
-    cout << "Choice --> ";
-    cin >> entry;
-
-    // traverse that many times and delete that node
-    current = head;
-    Node *prev = head;
-    for (int i = 0; i < (entry-1); i++)
-        if (i == 0)
-            current = current->next;
-        else {
-            current = current->next;
-            prev = prev->next;
-        }
-    // at this point, delete current and reroute pointers
-    if (current) {  // checks for current to be valid before deleting the node
-        prev->next = current->next;
-        delete current;
-        current = nullptr;
-    }
-    */
-    
     head = deleteNode(head);
     output(head);
     
-    
-    // insert a node
-    /*
-    current = head;
-    cout << "After which node to insert 10000? " << endl;
-    count = 1;
-    while (current) {
-        cout << "[" << count++ << "] " << current->value << endl;
-        current = current->next;
-    }
-    cout << "Choice --> ";
-    cin >> entry;
-
-    current = head;
-    prev = head;
-    for (int i = 0; i < (entry); i++)
-        if (i == 0)
-            current = current->next;
-        else {
-            current = current->next;
-            prev = prev->next;
-        }
-    //at this point, insert a node between prev and current
-    Node * newnode = new Node;
-    newnode->value = 10000;
-    newnode->next = current;
-    prev->next = newnode;
-    */
-    
     head = insertNode(head, 1000);
     output(head);
-    
-    /*
-    // deleting the linked list
-    current = head;
-    while (current) {
-        head = current->next;
-        delete current;
-        current = head;
-    }
-    head = nullptr;
-    */
-
+   
     head = deleteList(head);
     output(head);
 
@@ -136,6 +55,29 @@ Node * addFront(Node *headNode, float val){
         newVal->next = headNode;
         newVal->value = val;
         headNode = newVal;
+    }
+    cout << headNode->value << endl;
+    return headNode;
+}
+
+Node * addBack(Node * headNode, float val){
+    Node * newVal = new Node;
+    Node * current = headNode;
+    // adds node at head
+    if (!headNode) { // if this is the first node, it's the new head
+        newVal->next = nullptr;
+        newVal->value = val;
+        headNode = newVal;
+        cout << headNode->value << endl;
+    }
+    else {
+        while ((current->next)->next){ // traverses array till next points to nullptr
+            current = current->next;
+        }
+        newVal->next = nullptr;
+        newVal->value = val;
+        current = newVal;
+        cout << current->value << endl;
     }
     return headNode;
 }
